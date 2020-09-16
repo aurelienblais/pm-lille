9bba0b20c658abc0ce22ae797d1c9d3a5cb8efb2
Aurélien Blais <naritaya@naritaya.org>
2020-09-16 23:19:36 +0200

    Implement basic calendar

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class DashboardController < ApplicationController
   def index
+    @agents = Agent.order_by_name
+    @current_date = Date.today
+    @date_range = @current_date.beginning_of_month..@current_date.end_of_month
+
   end
 end
